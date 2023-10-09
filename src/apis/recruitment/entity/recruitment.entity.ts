82f14725b05ccf317967f6c6418ea928f3cfbd1c
developersomin <angiwon12345@nate.com>
2023-10-09 20:15:46 +0900

    Feat: "recruitment 엔티티 등록"

---
+++ src/apis/recruitment/entity/recruitment.entity.ts
@@ -0,0 +1,54 @@
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    ManyToOne, OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import {Field, Int, ObjectType} from "@nestjs/graphql";
+import {Company} from "../../company/entity/company.entity";
+import { UserRecruitment} from "../../userRecruitment/entity/user-recruitment.entity";
+
+@Entity()
+@ObjectType()
+export class Recruitment{
+
+    @PrimaryGeneratedColumn('uuid')
+    @Field(()=>String)
+    id: string;
+
+    @Column()
+    @Field(()=>String)
+    position: string;
+
+    @Column()
+    @Field(()=>Int)
+    reward: number;
+
+    @Column()
+    @Field(()=>String)
+    contents :string;
+
+    @Column()
+    @Field(()=>String)
+    stack: string;
+
+    @ManyToOne(()=>Company)
+    @Field(()=>Company)
+    company: Company;
+
+    @OneToMany(()=>UserRecruitment, userRecruitment=> userRecruitment.recruitment)
+    @Field(()=>[UserRecruitment])
+    userRecruitments: UserRecruitment[];
+
+    @CreateDateColumn()
+    createAt: Date;
+
+    @UpdateDateColumn()
+    updateAt: Date;
+
+    @DeleteDateColumn()
+    deleteAt: Date;
+}
