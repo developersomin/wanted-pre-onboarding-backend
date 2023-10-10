883acb7a46080927f1b1d54b027d2cea6391ac9d
developersomin <angiwon12345@nate.com>
2023-10-10 23:06:37 +0900

    Style: " 변경사항 없경 "

--- src/apis/recruitment/dto/detail-recruitment.output.ts
+++ src/apis/recruitment/dto/detail-recruitment.output.ts
@@ -1,9 +1,8 @@
-import {Field, Int, ObjectType, PartialType, PickType} from "@nestjs/graphql";
-import {Recruitment} from "../entity/recruitment.entity";
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Recruitment } from '../entity/recruitment.entity';
 
 @ObjectType()
-export class DetailRecruitmentOutput extends Recruitment{
-
-    @Field(()=>[String])
-    companyRecruitmentId: string[];
+export class DetailRecruitmentOutput extends Recruitment {
+  @Field(() => [String])
+  companyRecruitmentId: string[];
 }
